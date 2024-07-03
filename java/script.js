0712ef09953212f962437ad20791afa631e4773d
marco-scocca <marcoscocca1991@gmail.com>
2024-07-03 07:40:06 +0200

    ciclo array

---
+++ java/script.js
@@ -0,0 +1,8 @@
+let guests = ['marco', 'fabrizio'];
+document.getElementById('check-guest').addEventListener('click', function () {
+    console.log('ciao')
+    let name = document.getElementById('name').value;
+    for (let i = 0; i < guests.length; i++) {
+
+    }
+});

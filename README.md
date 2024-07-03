6ac1fdb5851ee15c8ca0d2df53120ec996f1bb6f
marco-scocca <marcoscocca1991@gmail.com>
2024-07-03 07:57:12 +0200

    readme snack 2

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ se sono uguali imposto variabile a true
 controllo valore variabile di controllo
 se è vero allora mostro messaggio benvenuto
 altrimenti dico che non è invitato
+
+snack2
+creo array
+eseguo ciclo di sei iterazioni
+eseguo prompt in cui utente inserisce numero
+se numero inserito è dispari inserire nell'array
+altrimenti non fa nulla
+mostra array
--- java/script.js
+++ java/script.js
@@ -1,10 +1,17 @@
 let guests = ['marco', 'fabrizio'];
+let check_guest = false
 document.getElementById('check-guest').addEventListener('click', function () {
-    console.log('ciao')
+
     let name = document.getElementById('name').value;
     for (let i = 0; i < guests.length; i++) {
         if (guests[i], toLowerCase() === name.toLowerCase()) {
             check_guest = true
         }
+    }
+    if (check_guest) {
+        console.log('Benvenuto ${name}, si sieda');
+    }
+    else {
+        console.log('non siete in lista');
     }
 });
